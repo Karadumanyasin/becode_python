0bad2d68c2f49f511699764da1e6d10aa95f3410
Karadumanyasin <karayasinduman@gmail.com>
2024-10-17 16:29:17 +0200

    ajout exo built in fonction

--- becode_python.py
+++ becode_python.py
@@ -61,3 +61,61 @@ elif argent_restant < 0:
 else:
     message = "Vous êtes fauché !"
 print(message)
+
+
+import math
+count_alpha = len("Hello world!")
+count_float = float(count_alpha)
+round_pi = round(math.pi, 2)
+text = "Hello world!"
+reversed_text = list(text[::-1])
+print(reversed_text)
+age = input("quel est votre age ? :")
+print("votre age est " + age + "ans")
+print(type(age))
+num = [ 2, 8, 1, 4, 6, 3, 7]
+sorted_num = sorted(num)
+sum_of_list = sum(num)
+min_value = min(num)
+max_value = max(num)
+calc = "1 + 2"
+string_interpret = eval(calc)
+print(string_interpret)
+import unittest
+
+
+class TestNotebook(unittest.TestCase):
+    def test_count_alpha(self):
+        self.assertEqual(count_alpha, 12)
+
+    def test_count_float(self):
+        self.assertEqual(type(count_float), float)
+
+    def test_pi(self):
+        self.assertEqual(3.14, round_pi)
+
+    def test_reversed(self):
+        self.assertEqual(
+            reversed_text, ["!", "d", "l", "r", "o", "w", " ", "o", "l", "l", "e", "H"]
+        )
+
+    def test_age(self):
+        self.assertEqual(type(age), str)
+
+    def test_sorted(self):
+        self.assertEqual(sorted_num, [1, 2, 3, 4, 6, 7, 8])
+
+    def test_sum(self):
+        self.assertEqual(sum_of_list, 31)
+
+    def test_min(self):
+        self.assertEqual(min_value, 1)
+
+    def test_max(self):
+        self.assertEqual(max_value, 8)
+
+    def test_interpret(self):
+        self.assertEqual(string_interpret, 3)
+
+
+unittest.main(argv=[""], verbosity=2, exit=False)
